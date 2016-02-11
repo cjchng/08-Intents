96570e7707bb7d17b9975b77e99196b807a7d36b
Jim Pepe <jim.pepe@verizon.net>
2016-02-10 21:21:08 -0500

    clean up

--- app/src/main/java/com/course/example/intents/Intents.java
+++ app/src/main/java/com/course/example/intents/Intents.java
@@ -18,7 +18,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Intents extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */ 
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
